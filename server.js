51f248f9bc4f62471e9412a7664bba44cf24bfc7
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-14 12:19:59 +0200

    Remove unused dependecies

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const app = express();
 const pathToBuildFolder = path.join(__dirname, 'build');
 const pathToIndexHtml = path.join(pathToBuildFolder, 'index.html');
 
-app.use(express.static(pathToBuildFolder));
-
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}${req.url}`);
     else next();
   });
 }
+app.use(express.static(pathToBuildFolder));
 
 app.get('/', (req, res) => {
   res.sendFile(pathToIndexHtml);
