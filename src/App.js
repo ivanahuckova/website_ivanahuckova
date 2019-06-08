718190a569705ed2ebcdba9289c7657a9204d6b9
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-08 20:48:43 +0200

    Fix rendering svg over text

--- src/App.js
+++ src/App.js
@@ -3,50 +3,50 @@ import './App.scss';
 import person from './imgs/person.svg';
 
 function App() {
-  return (
-    <div className="App svg-animation">
-      <div className="container">
-        <div className="heading">
-          <div className="person-container">
-            <img className="person" src={person} alt="person" />
-          </div>
-          <div className="content">
-            <h1>Hello!</h1>
-            <div>
-              My name is Ivana and I am a full-stack JavaScript Developer in Bratislava. You can check out my{' '}
-              <a href="https://github.com/ivanahuckova" target="_blank" rel="noopener noreferrer">
-                github
-              </a>
-              ,{' '}
-              <a href="https://twitter.com/ivanahuckova" target="_blank" rel="noopener noreferrer">
-                twitter
-              </a>
-              ,{' '}
-              <a href="https://www.linkedin.com/in/ivana-huckova/" target="_blank" rel="noopener noreferrer">
-                linkedin
-              </a>{' '}
-              and{' '}
-              <a href="https://medium.com/@ivanahuckova" target="_blank" rel="noopener noreferrer">
-                medium
-              </a>
-              . I am currently working on{' '}
-              <a href="https://www.hackersalt.com" target="_blank" rel="noopener noreferrer">
-                hackersalt
-              </a>{' '}
-              and{' '}
-              <a href="https://pinely.app" target="_blank" rel="noopener noreferrer">
-                pinely.
-              </a>
-              <br />
-              <br />
-              Email me at <a href="mailto: hello@ivanahuckova.com">hello@ivanahuckova.com</a>
+    return (
+        <div className="App svg-animation">
+            <div className="container">
+                <div className="heading">
+                    <div className="person-container">
+                        <img className="person" src={person} alt="person" />
+                    </div>
+                    <div className="content">
+                        <h1>Hello!</h1>
+                        <div>
+                            My name is Ivana and I am a Fullstack JavaScript Developer in Bratislava. You can check out my{' '}
+                            <a href="https://github.com/ivanahuckova" target="_blank" rel="noopener noreferrer">
+                                github
+                            </a>
+                            ,{' '}
+                            <a href="https://twitter.com/ivanahuckova" target="_blank" rel="noopener noreferrer">
+                                twitter
+                            </a>
+                            ,{' '}
+                            <a href="https://www.linkedin.com/in/ivana-huckova/" target="_blank" rel="noopener noreferrer">
+                                linkedin
+                            </a>{' '}
+                            and{' '}
+                            <a href="https://medium.com/@ivanahuckova" target="_blank" rel="noopener noreferrer">
+                                medium
+                            </a>
+                            . I am currently working on{' '}
+                            <a href="https://www.hackersalt.com" target="_blank" rel="noopener noreferrer">
+                                hackersalt
+                            </a>{' '}
+                            and{' '}
+                            <a href="https://pinely.app" target="_blank" rel="noopener noreferrer">
+                                pinely.
+                            </a>
+                            <br />
+                            <br />
+                            Email me at <a href="mailto: hello@ivanahuckova.com">hello@ivanahuckova.com</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className="svg-animation" />
         </div>
-      </div>
-      <div className="svg-animation" />
-    </div>
-  );
+    );
 }
 
 export default App;
